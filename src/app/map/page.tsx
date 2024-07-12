c5033357a13015e3657494f3af4d4327481e5059
hatbann <0urloveis_great@naver.com>
2024-07-12 15:54:54 +0900

    (feat) 지도 이동시 다시 검색하는 로직 추가

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -28,60 +28,71 @@ const page = () => {
 
     const onLoadKakaoAPI = () => {
       window.kakao.maps.load(() => {
-        const container = document.getElementById('map');
-        const options = {
-          center: new window.kakao.maps.LatLng(
-            location.coords.latitude,
-            location.coords.longitude
-          ),
-          level: 3,
-        };
-
-        const map = new window.kakao.maps.Map(container, options);
-        setMap(map);
-        const ps = new window.kakao.maps.services.Places(map);
-        const infowindow = new window.kakao.maps.InfoWindow({ zIndex: 1 });
-
-        // 카테고리로 은행을 검색합니다
-        ps.categorySearch('FD6', placesSearchCB, { useMapBounds: true });
-
-        // 키워드 검색 완료 시 호출되는 콜백함수 입니다
-        function placesSearchCB(data: any, status: any, pagination: any) {
-          console.log(status);
-          if (status === window.kakao.maps.services.Status.OK) {
-            for (let i = 0; i < data.length; i++) {
-              console.log(data);
-              displayMarker(data[i]);
-            }
-          }
-        }
-
-        function displayMarker(place: any) {
-          // 마커를 생성하고 지도에 표시합니다
-          var marker = new window.kakao.maps.Marker({
-            map: map,
-            position: new window.kakao.maps.LatLng(place.y, place.x),
-          });
-
-          // 마커에 클릭이벤트를 등록합니다
-          window.kakao.maps.event.addListener(marker, 'click', function () {
-            // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
-            infowindow.setContent(
-              '<div style="padding:5px;font-size:12px;">' +
-                place.place_name +
-                '</div>'
-            );
-            infowindow.open(map, marker);
-          });
-        }
+        tempF(location.coords.latitude, location.coords.longitude);
       });
     };
 
     kakaoMapScript.addEventListener('load', onLoadKakaoAPI);
   };
 
+  const tempF = (lat: number, lng: number) => {
+    const container = document.getElementById('map');
+    const options = {
+      center: new window.kakao.maps.LatLng(lat, lng),
+      level: 3,
+    };
+
+    const map = new window.kakao.maps.Map(container, options);
+    setMap(map);
+    const ps = new window.kakao.maps.services.Places(map);
+    const infowindow = new window.kakao.maps.InfoWindow({ zIndex: 1 });
+
+    // 카테고리로 은행을 검색합니다
+    ps.categorySearch('FD6', placesSearchCB, { useMapBounds: true });
+
+    // 키워드 검색 완료 시 호출되는 콜백함수 입니다
+    function placesSearchCB(data: any, status: any, pagination: any) {
+      console.log(status);
+      if (status === window.kakao.maps.services.Status.OK) {
+        for (let i = 0; i < data.length; i++) {
+          displayMarker(data[i]);
+        }
+      }
+    }
+
+    function displayMarker(place: any) {
+      // 마커를 생성하고 지도에 표시합니다
+      var marker = new window.kakao.maps.Marker({
+        map: map,
+        position: new window.kakao.maps.LatLng(place.y, place.x),
+      });
+
+      // 마커에 클릭이벤트를 등록합니다
+      window.kakao.maps.event.addListener(marker, 'click', function () {
+        // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
+        infowindow.setContent(
+          '<div style="padding:5px;font-size:12px;">' +
+            place.place_name +
+            '</div>'
+        );
+        infowindow.open(map, marker);
+      });
+    }
+  };
+  const handleCenterChanged = (map: any) => {
+    const level = map.getLevel();
+
+    // 지도의 중심좌표를 얻어옵니다
+    const latlng = map.getCenter();
+    console.log(latlng);
+    tempF(latlng.Ma, latlng.La);
+  };
+
   useEffect(() => {
     if (map) {
+      window.kakao.maps.event.addListener(map, 'dragend', () =>
+        handleCenterChanged(map)
+      );
     }
   }, [map]);
 
